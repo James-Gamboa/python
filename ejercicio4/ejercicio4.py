313f351330eedb40358b834180243ce3a274b6bf
James <jjguevarag@gmail.com>
2025-04-08 11:37:10 -0600

    Fix input handling in ejercicio3 and implement vowel counting in ejercicio4

--- ejercicio4/ejercicio4.py
+++ ejercicio4/ejercicio4.py
@@ -1,3 +1,9 @@
 # Ejercicio 4
-
 # Escribe un programa en Python que cuente cuántas vocales hay en una frase ingresada por el usuario.
+
+list_vowels = input("ingrese una frase: ")
+vowels = ["a", "e", "i", "o", "u"]
+
+for i in list_vowels:
+    if i in vowels:
+        print(i)
